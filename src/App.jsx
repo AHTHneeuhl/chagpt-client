0a317f9866628ab4e8cab8398d60e534331ad043
Shubham Patrick <ahthneeuhl@gmail.com>
2023-03-07 21:03:13 +0530

    Main Chat App and React Chat Engine Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="app">App</div>;
-}
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
+
+const App = () => {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
