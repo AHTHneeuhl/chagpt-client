9920db99f0822f2d0332cfb27542f896da767c7d
Shubham Patrick <ahthneeuhl@gmail.com>
2023-03-08 05:44:44 +0530

    Redux-Toolkit Setup and create Ai and MessageFormUI components

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -5,6 +5,7 @@ import {
 } from "react-chat-engine-advanced";
 import Header from "@/components/header";
 import StandardMessageForm from "@/components/messageForms/StandardMessageForm";
+import Ai from "@/components/messageForms/Ai";
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(
@@ -21,6 +22,9 @@ const Chat = () => {
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
         renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
           return (
             <StandardMessageForm props={props} activeChat={chatProps.chat} />
           );
--- src/components/messageForms/Ai.jsx
+++ src/components/messageForms/Ai.jsx
@@ -1,9 +1,11 @@
+import { usePostAiTextMutation } from "@/state/api";
 import { useState } from "react";
 import MessageFormUi from "./MessageFormUi";
 
-const StandardMessageForm = ({ props, activeChat }) => {
+const Ai = ({ props, activeChat }) => {
   const [message, setMessage] = useState("");
   const [attachment, setAttachment] = useState("");
+  const [trigger] = usePostAiTextMutation();
 
   const handleChange = (e) => setMessage(e.target.value);
 
@@ -22,6 +24,7 @@ const StandardMessageForm = ({ props, activeChat }) => {
     };
 
     props.onSubmit(form);
+    trigger(form);
     setMessage("");
     setAttachment("");
   };
@@ -36,4 +39,4 @@ const StandardMessageForm = ({ props, activeChat }) => {
   );
 };
 
-export default StandardMessageForm;
+export default Ai;
